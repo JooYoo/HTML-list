7919c96ba65b37bd59f6cc3afd74870002d6a8bb
JooYoo <jooyoo@outlook.com>
2018-05-23 09:55:30 +0200

    remove button works

--- js/listManager.js
+++ js/listManager.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
     var $testLi = $('.testLi'),
-        $btnDone = $('.btnDone');
+        $btnDone = $('.btnDone'),
+        $btnNotDone = $('.btnNotDone');
 
-    function addStrick() {
+
+    function addStrike() {
         // get the traget element
         var liText = $testLi.html();
         // add tag to the target element
-        liText = '<strike>' + liText + '</strike>';
+        liText = '<strike class="strikeText">' + liText + '</strike>';
         // add the result back to DOM
         $testLi.html(liText);
     }
 
+    function removeStrike(){
+        //in <li> find <strike> contents;
+        // unwrap() 去除找到的内容的父标签
+        $testLi.find('strike').contents().unwrap();
+    }
+
     $btnDone.on('click', function () {
-        addStrick();
+        addStrike();
+    });
+
+    $btnNotDone.on('click', function(){
+        removeStrike();
     });
 });
